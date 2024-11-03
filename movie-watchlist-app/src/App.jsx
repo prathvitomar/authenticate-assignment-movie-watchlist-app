5dedf72d4cb3acd51170486b5039736b70825e2e
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-11-03 18:20:06 +0530

    Created Auth for Login and User Verification

--- movie-watchlist-app/src/App.jsx
+++ movie-watchlist-app/src/App.jsx
@@ -2,50 +2,39 @@ import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch} from "react-redux";
 import { useEffect, useState } from "react";
-import { restoreUser } from "./features/users/userSlice";
-import { restoreAuth } from "./features/auth/authSlice";
-import { ThemeProvider } from "./features/useTheme/Theme";
+import authService from "./appwrite/auth/auth"
+import Loading from "./components/ui/Loading/Loading";
+import { login, logout } from "./appwrite/slices/authSlice";
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
-  const [mode, setMode] = useState("light")
-  const authState = useSelector((state) => state.auth);
-
-  const lightTheme = () =>{
-    setMode("light");
-  }
-
-  const darkTheme = () =>{
-    setMode("dark");
-  }
 
   useEffect(() => {
-    const activeUser = JSON.parse(localStorage.getItem("activeUser"));
-    if (activeUser) {
-      dispatch(restoreUser());
-      dispatch(restoreAuth({ email: activeUser.email }));
-    }
-    console.log("Active User from userSlice:", activeUser);
-    console.log("Auth state:", authState);
-  }, [dispatch, authState]);
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  useEffect(() => {
-    document.querySelector('html').classList.remove("light", "dark")
-    document.querySelector('html').classList.add(mode)
-  }, [mode])
-
-  return (
+  return !loading ? (
     <>
-      <ThemeProvider value={{mode, lightTheme, darkTheme}}>
         <Navbar />
         <div className="my-6">
           <Outlet />
         </div>
         <Footer />
-      </ThemeProvider>
     </>
+  ) : (
+    <Loading />
   );
 }
 
--- movie-watchlist-app/src/app/store/store.js
+++ movie-watchlist-app/src/app/store/store.js
@@ -1,27 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  moviesSlice  from "../../features/movies/moviesSlice";
-import  userSlice  from "../../features/users/userSlice";
-import  authSlice  from "../../features/auth/authSlice";
+import moviesSlice from "../../features/movies/moviesSlice";
+import userSlice from "../../features/users/userSlice";
+import authSlice from "../../appwrite/auth/auth";
 
-const persistedUser = JSON.parse(localStorage.getItem('activeUser'));
+const persistedUser = JSON.parse(localStorage.getItem("activeUser"));
 const persistedEmail = persistedUser ? persistedUser.email : null;
 
 const store = configureStore({
-    reducer : {
-        movies : moviesSlice,
-        user : userSlice,
-        auth : authSlice
+  reducer: {
+    auth: authSlice,
+    movies: moviesSlice,
+    user: userSlice,
+  },
+  preloadedState: {
+    user: {
+      activeUser: persistedUser,
+      users: JSON.parse(localStorage.getItem("users")) || [],
     },
-    preloadedState: {
-        user: {
-          activeUser: persistedUser,
-          users: JSON.parse(localStorage.getItem('users')) || [],
-        },
-        auth: {
-          email: persistedEmail,
-          isAuthenticated: !!persistedEmail,
-        },
-    },   
-})
+    auth: {
+      email: persistedEmail,
+      isAuthenticated: !!persistedEmail,
+    },
+  },
+});
 
 export default store;
